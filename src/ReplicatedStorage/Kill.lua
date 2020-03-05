0fbae21ddeb55f615f7214b9b50943e6cba1eabc
Jovanni Cutigni <jcutigni@gmail.com>
2020-03-05 14:34:59 -0800

    Simplify test obby kill script

--- src/ReplicatedStorage/Kill.lua
+++ src/ReplicatedStorage/Kill.lua
@@ -1,11 +1,15 @@
 local CollectionService = game:GetService("CollectionService")
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local localPlayer = Players.LocalPlayer
+local isServer = RunService:IsServer()
 
 function register(instance)
 	instance.Touched:Connect(function(part)
 		local model = part:FindFirstAncestorWhichIsA("Model")
 		local humanoid = model and model:FindFirstChild("Humanoid")
-		if humanoid then
+		if humanoid and (isServer or Players:GetPlayerFromCharacter(model) == localPlayer) then
 			humanoid.Health = 0
 		end
 	end)
@@ -15,45 +19,5 @@ for _, instance in ipairs(CollectionService:GetTagged("Kill")) do
 	register(instance)
 end
 CollectionService:GetInstanceAddedSignal("Kill"):Connect(register)
-
--- Set player Humanoid properties:
-local function onHumanoidAdded(character, humanoid)
-	humanoid.Touched:Connect(function(touchingPart, humanoidPart)
-		if CollectionService:HasTag(touchingPart, "Kill") then
-			humanoid.Health = 0
-		end
-	end)
-end
-
--- Track existing and new player Humanoids:
-local function onCharacterAdded(character)
-	local humanoid = character:FindFirstChildOfClass("Humanoid")
-	if humanoid then
-		onHumanoidAdded(character, humanoid)
-	else
-		character.ChildAdded:Connect(
-			function(child)
-				if child:IsA("Humanoid") then
-					onHumanoidAdded(character, child)
-				end
-			end
-		)
-	end
-end
-
--- Track existing and new player characters:
-local function onPlayerAdded(player)
-	player.CharacterAdded:Connect(onCharacterAdded)
-	if player.Character then
-		onCharacterAdded(player.Character)
-	end
-end
-
-local player = Players.LocalPlayer
-if player then
-	onPlayerAdded(player)
-else
-	Players.PlayerAdded:Connect(onPlayerAdded)
-end
 
 return nil
