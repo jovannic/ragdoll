1673d6a09ce13aa039a056bbe51eec3f742ba131
Jovanni Cutigni <jcutigni@gmail.com>
2020-03-05 11:35:57 -0800

    Revise comments and add more comments
    The network ownership and animation velocity timing stuff is really
    finicky and requires me to do unholy, fragile things that require
    justification. I'm committing these crimes for a good cause, I swear.

--- src/ReplicatedStorage/Ragdoll/init.lua
+++ src/ReplicatedStorage/Ragdoll/init.lua
@@ -63,29 +63,32 @@ end
 local function onOwnedHumanoidDeath(character, humanoid)
 	-- We first break the motors on the network owner (the player that owns this character).
 	--
-	-- If we initiated ragdoll by breaking joints on the server there's a visible hitch while the
-	-- server waits at least a full round trip time for the network owner to receive the joint
-	-- removal, start simulating the ragdoll, and replicating physics state. Meanwhile the other
-	-- body parts would be frozen in air on the server and other clients until there is physics data
-	-- from the owner.
-	--
 	-- This way there is no visible round trip hitch. By the time the server receives the joint
 	-- break physics data for the child parts should already be available. Seamless transition.
 	--
-	-- We also specifically do not break the root joint so we keep a consistent mechanism / network
-	-- ownership unit root. If we did break the root joint we'd be creating a new, seperate network
-	-- onwership unit that we would have to wait for the server to assign us ownership of before we
-	-- would start simulating and replicating physics data for it, creating an additional hitch.
+	-- If we initiated ragdoll by breaking joints on the server there's a visible hitch while the
+	-- server waits at least a full round trip time for the network owner to receive the joint
+	-- removal, start simulating the ragdoll, and replicate physics data. Meanwhile the other body
+	-- parts would be frozen in air on the server and other clients until physics data arives from
+	-- the owner. The ragdolled player wouldn't see it, but other players would.
+	--
+	-- We also specifically do not break the root joint on the client so we can maintain a
+	-- consistent mechanism and network ownership unit root. If we did break the root joint we'd be
+	-- creating a new, seperate network onwership unit that we would have to wait for the server to
+	-- assign us network ownership of before we would start simulating and replicating physics data
+	-- for it, creating an additional round trip hitch on our end for our own character.
 	local motors = Rigging.breakMotors(character, humanoid.RigType)
 
 	-- Apply velocities from animation to the child parts to mantain visual momentum.
 	--
-	-- This should be done on the network owner's side just after disabling the kinematic
-	-- joint so the child parts are split off as seperate dynamic bodies.
+	-- This should be done on the network owner's side just after disabling the kinematic joint so
+	-- the child parts are split off as seperate dynamic bodies. For consistent animation times and
+	-- visual momentum we want to do this on the machine that controls animation state for the
+	-- character and will be simulating the ragdoll, in this case the client.
 	--
-	-- It's also important that this is called *before* any animations are canceled or
-	-- changed after death! Otherwise there will be no animations to get velocities from or
-	-- the velocities won't be consistent!
+	-- It's also important that this is called *before* any animations are canceled or changed after
+	-- death! Otherwise there will be no animations to get velocities from or the velocities won't
+	-- be consistent!
 	local animator = humanoid:FindFirstChildWhichIsA("Animator")
 	if animator then
 		animator:ApplyJointVelocities(motors)
@@ -108,8 +111,11 @@ end
 -- Handle Humanoid death
 local function onHumanoidAdded(player, character, humanoid)
 	if isServer then
+		-- Server creates ragdoll joints on spawn to allow for seamless transition later.
 		Rigging.createRagdollJoints(character, humanoid.RigType)
+		-- We will only disable specific joints
 		humanoid.BreakJointsOnDeath = false
+		-- We don't bother with fade-out on the server
 	else
 		-- Any character: handle fade out on death
 		humanoid.Died:Connect(function()
