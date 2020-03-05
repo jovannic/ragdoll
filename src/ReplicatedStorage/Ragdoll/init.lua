bedf6e9f98d3465622ce6d290626a9d9ef1af9b1
Jovanni Cutigni <jcutigni@gmail.com>
2020-03-04 19:06:20 -0800

    Make clients fade out other characters
    To avoid adding another remote event, have other players listen for
    Humanoid.Died on each other's characters.

--- src/ReplicatedStorage/Ragdoll/init.lua
+++ src/ReplicatedStorage/Ragdoll/init.lua
@@ -73,11 +73,13 @@ if RunService:IsServer() then
 
 else -- Client
 
+	local localPlayer = Players.LocalPlayer
+
 	local function disableParticleEmittersAndFadeOut(descendants, duration)
 		local transparencies = {}
 		for _, instance in pairs(descendants) do
 			if instance:IsA("BasePart") or instance:IsA("Decal") then
-				table.insert(transparencies, { instance, instance.Transparency })
+				table.insert(transparencies, { instance, instance.LocalTransparencyModifier })
 			end
 			if instance:IsA("ParticleEmitter") then
 				instance.Enabled = false
@@ -92,7 +94,7 @@ else -- Client
 			local alpha = math.min(t / duration, 1)
 			for _, pair in pairs(transparencies) do
 				local p, a = unpack(pair)
-				p.Transparency = (1 - alpha) * a + alpha * 1
+				p.LocalTransparencyModifier = (1 - alpha) * a + alpha * 1
 			end
 		end
 	end
@@ -122,56 +124,64 @@ else -- Client
 			end
 	end
 
-	-- Set player Humanoid properties:
-	local function onHumanoidAdded(character, humanoid)
+	local function onOwnedHumanoidDeath(character, humanoid)
+		-- We first break the motors on the network owner (character's player in this case). If
+		-- we initiated ragdoll by breaking joints on the server there's a visible hitch while
+		-- the server waits, a full round trip latency delay at least, for the network owner to
+		-- recieve the joint removal, start simulating the ragdoll, and replicating physics
+		-- state. so that there is no visible round trip hitch while the server is waiting for
+		-- physics replication data for the child body parts that the owner (client) hasn't
+		-- simulated yet. This way by the time the server recieves the joint break physics data
+		-- for the child parts should already be available.
+		local motors = Rigging.disableMotors(character, humanoid.RigType)
+	
+		-- Apply velocities from animation to the child parts to mantain visual momentum.
+		--
+		-- This should be done on the network owner's side just after disabling the kinematic
+		-- joint so the child parts are split off as seperate dynamic bodies.
+		--
+		-- It's also important that this is called *before* any animations are canceled or
+		-- changed after death! Otherwise there will be no animations to get velocities from or
+		-- the velocities won't be consistent!
+		local animator = humanoid:FindFirstChildWhichIsA("Animator")
+		if animator then
+			animator:ApplyJointVelocities(motors)
+		end
+	
+		-- stiff shock phase...
+		wait(0.1)
+	
+		-- gradually give up...
+		easeJointFriction(character:GetDescendants(), 0.5)
+	end
+
+	-- Handle Humanoid death
+	local function onHumanoidAdded(player, character, humanoid)
+		-- Any character: handle fade out on death
 		humanoid.Died:Connect(function()
-			-- We first break the motors on the network owner (character's player in this case). If
-			-- we initiated ragdoll by breaking joints on the server there's a visible hitch while
-			-- the server waits, a full round trip latency delay at least, for the network owner to
-			-- recieve the joint removal, start simulating the ragdoll, and replicating physics
-			-- state. so that there is no visible round trip hitch while the server is waiting for
-			-- physics replication data for the child body parts that the owner (client) hasn't
-			-- simulated yet. This way by the time the server recieves the joint break physics data
-			-- for the child parts should already be available.
-			local motors = Rigging.breakMotors(character, humanoid.RigType)
-
-			-- Apply velocities from animation to the child parts to mantain visual momentum.
-			--
-			-- This should be done on the network owner's side just after disabling the kinematic
-			-- joint so the child parts are split off as seperate dynamic bodies.
-			--
-			-- It's also important that this is called *before* any animations are canceled or
-			-- changed after death! Otherwise there will be no animations to get velocities from or
-			-- the velocities won't be consistent!
-			local animator = humanoid:FindFirstChildWhichIsA("Animator")
-			if animator then
-				animator:ApplyJointVelocities(motors)
-			end
-
-			-- stiff shock phase...
-			wait(0.1)
-
-			-- gradually give up...
-			easeJointFriction(character:GetDescendants(), 0.5)
-
-			-- time to settle...
-			wait(0.6)
-
+			-- time for friction fade out and settle
+			wait(1.5)
 			-- fade into the mist...
 			disableParticleEmittersAndFadeOut(character:GetDescendants(), 0.6)
 		end)
+		-- Just my character: initiate ragdoll and do friction easing 
+		if player == localPlayer then
+			humanoid.Died:Connect(function()
+				onOwnedHumanoidDeath(character, humanoid)
+			end)
+		end
 	end
 
 	-- Track existing and new player Humanoids:
-	local function onCharacterAdded(character)
+	local function onCharacterAdded(player, character)
 		local humanoid = character:FindFirstChildOfClass("Humanoid")
 		if humanoid then
-			onHumanoidAdded(character, humanoid)
+			onHumanoidAdded(player, character, humanoid)
 		else
 			character.ChildAdded:Connect(
 				function(child)
 					if child:IsA("Humanoid") then
-						onHumanoidAdded(character, child)
+						onHumanoidAdded(player, character, child)
 					end
 				end
 			)
@@ -180,14 +190,21 @@ else -- Client
 
 	-- Track existing and new player characters:
 	local function onPlayerAdded(player)
-		player.CharacterAdded:Connect(onCharacterAdded)
-		if player.Character then
-			onCharacterAdded(player.Character)
+		player.CharacterAdded:Connect(function(character)
+			onCharacterAdded(player, character)
+		end)
+		local character = player.Character
+		if character then
+			onCharacterAdded(player, character)
 		end
 	end
 
-	onPlayerAdded(Players.LocalPlayer)
-
+	-- Track all players (including local player):
+	Players.PlayerAdded:Connect(onPlayerAdded)
+	for _, player in pairs(Players:GetPlayers()) do
+		onPlayerAdded(player)
+	end
+	
 	-- Other API not available from client-side:
 
 	function Ragdoll:SetPlayerDefault()
